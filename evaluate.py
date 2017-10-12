64dd4bf449dd2980c51cca8d919def2c1266fa41
hellochick <hellochick@github.com>
2017-10-12 17:42:42 +0800

    add fm

--- evaluate.py
+++ evaluate.py
@@ -44,13 +44,20 @@ def main():
     softmax = tf.nn.softmax(fc_out)
     pred = tf.argmax(softmax, axis=-1)
 
+    # convert label to onehot
+    gt = tf.one_hot(indices=label_batch, depth=NUM_CLASSES)
+
+    mIoU, update_op = tf.contrib.metrics.streaming_mean_iou(pred, label_batch, num_classes=NUM_CLASSES)
+
     # Set up tf session and initialize variables.
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
-    init = tf.global_variables_initializer()
+    global_init = tf.global_variables_initializer()
+    local_init = tf.local_variables_initializer()
 
-    sess.run(init)
+    sess.run(global_init)
+    sess.run(local_init)
 
     # Load weights.
     loader = tf.train.Saver(var_list=restore_var)
@@ -70,7 +77,7 @@ def main():
 
     true_ct = 0
     for step in range(NUM_STEPS):
-        _pred, _label = sess.run([pred, label_batch])
+        _pred, _label, _cm = sess.run([pred, label_batch, update_op])
 
         if _pred == _label:
             true_ct += 1
@@ -79,6 +86,8 @@ def main():
 
     print('correct: {}'.format(true_ct))
     print('acc: {}'.format((float)(true_ct)/NUM_STEPS))
+    np.save('./results/cm.npy', _cm)
+
     coord.request_stop()
     coord.join(threads)
 
