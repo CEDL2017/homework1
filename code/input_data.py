de5a9388e8755eff5c1f0af146431219244a9fac
Chiang, Kai-Sheng <peterallpeterall@gmail.com>
2017-10-15 03:43:42 +0800

    Update input_data.py

--- code/input_data.py
+++ code/input_data.py
@@ -10,18 +10,9 @@ import numpy as np
 import os
 import re
 
-#%%
-
-# you need to change this to your data directory
-#train_dir = 'Lhand/'
 
 def read_data(file_dir, label_dir, train = True):
-    '''
-    Args:
-        file_dir: file directory
-    Returns:
-        list of images and labels
-    '''
+  
     image_list = []
     image_temp = []
 #    temp = []
@@ -31,18 +22,11 @@ def read_data(file_dir, label_dir, train = True):
             image_temp.append(os.path.join(root, name))
         image_list = image_list + sorted(image_temp, key = lambda x: int(re.sub('\D', '', x)))
         image_temp = []
-        # get 10 sub-folder names
-#        for name in direc:
-#            temp.append(os.path.join(root, name))
-    
-#    print(image_list)
-#    temp = []  
+       
     label_list = []  
     for root, direc, files in os.walk(label_dir):
 #        print(root)
-#        print(root == '/home/viplab/Desktop/petersci/CEDL_HW1/data/labels/lab')
-        if train:
-#            print("aaaaa")   
+        if train:   
             if root == '/home/viplab/Desktop/petersci/CEDL_HW1/data/labels/lab':
 #                print("111")
                 for i in range(1, 5):
@@ -94,8 +78,6 @@ def read_data(file_dir, label_dir, train = True):
     return image_list, label_list
 
 
-#%%
-
 def batch_generate(image, label, image_W, image_H, batch_size, capacity):
     
     image = tf.cast(image, tf.string)
@@ -107,8 +89,6 @@ def batch_generate(image, label, image_W, image_H, batch_size, capacity):
     label = input_queue[1]
     image_contents = tf.read_file(input_queue[0])
     image = tf.image.decode_png(image_contents, channels=3)
-#    print(image)
-#    image = tf.image.resize_image_with_crop_or_pad(image, image_W, image_H)
     image = tf.image.resize_images(image, [image_W, image_H])
     image = tf.image.per_image_standardization(image)  
     image_batch, label_batch = tf.train.batch([image, label],
@@ -121,8 +101,5 @@ def batch_generate(image, label, image_W, image_H, batch_size, capacity):
     label_batch = tf.reshape(label_batch, [batch_size, 24])
     image_batch = tf.cast(image_batch, tf.float32)
     
-    
-#    label_batch = tf.reshape(label_batch, [batch_size])
-#    image_batch = tf.cast(image_batch, tf.float32)
     return image_batch, label_batch
 
